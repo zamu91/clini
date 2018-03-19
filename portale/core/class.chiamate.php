6fc1092e946a8a2f18a9f9cbbbde241e1588bbf6
zamu <zamu@localhost.localdomain>
2018-03-19 17:48:06 +0100

    debug

--- portale/core/class.chiamate.php
+++ portale/core/class.chiamate.php
@@ -1,9 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 echo "start inc lib ";
 $dir=__DIR__."/";
-include_once $dir."trait.arxivar.php";
+
 echo "esempio ".$dir."trait.arxivar.php";
+die;
+include_once $dir."trait.arxivar.php";
 
 include_once $dir."trait.login.php";
 include_once $dir."trait.contratto.php";
@@ -22,9 +28,7 @@ class chiamate{
 
   public function debug(){
     $this->debug=true;
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+
   }
 
   public function __construct(){
