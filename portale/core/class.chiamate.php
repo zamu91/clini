09ec9c7584ae3a73d4b88916c9ddbd9eed0102b9
zamu <zamu@localhost.localdomain>
2018-03-19 17:06:52 +0100

    attivazione launcher per primo test

--- portale/core/class.chiamate.php
+++ portale/core/class.chiamate.php
@@ -11,51 +11,59 @@ include_once $dir."trait.prenotazione.php";
 
 class chiamate{
   use login,contratto,clinica,prenotazione,arxiva,sql;
-    private $conn; //connessione oracle
-    private $jsonMess;
+  private $conn; //connessione oracle
+  private $jsonMess;
+  private $debug;
+
+  public function debug(){
+    $this->debug=true;
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+  }
+
+  public function __construct(){
+    $this->debug();
+    require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Login.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Dati.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Search.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Documenti.php");
+  }
 
 
-    public function __construct(){
-        require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Login.php");
-        require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Dati.php");
-        require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Search.php");
-        require($_SERVER["DOCUMENT_ROOT"]."/Arxivar/ARX_Documenti.php");
+  public function halt(){
+    echo json_encode($this->jsonMess);
+    die;
+  }
+
+  public function setJsonMess($var,$mess,$dupli=1){
+    if($dupli=='1'){
+      $this->jsonMess[$var].=$mess;
+    }else{
+      $this->jsonMess[$var]=$mess;
+    }
+  }
+
+
+  public function post($nome){
+    if(empty($_POST[$nome])){
+      $this->setJsonMess('warn',"$nome non trovato");
+      return false;
+    }else{
+      return $_POST[$nome];
+    }
+  }
+
+  public function launcher(){
+    $azione=$this->post('azione');
+    switch($azione){
+      case 'loginPatrocinatore':
+      $this->loginArxivar();
+      break;
+
     }
 
-
-    public function halt(){
-        echo json_encode($this->jsonMess);
-        die;
-    }
-
-    public function setJsonMess($var,$mess,$dupli=1){
-        if($dupli=='1'){
-            $this->jsonMess[$var].=$mess;
-        }else{
-            $this->jsonMess[$var]=$mess;
-        }
-    }
-
-
-    public function post($nome){
-        if(empty($_POST[$nome])){
-            $this->setJsonMess('warn',"$nome non trovato");
-            return false;
-        }else{
-            return $_POST[$nome];
-        }
-    }
-
-    public function launcher(){
-      $azione=$this->post('azione');
-      switch($azione){
-        case 'loginPatrocinatore':
-          $this->loginArxivar();
-        break;
-
-      }
-
-    }
+  }
 
 
 
@@ -66,10 +74,11 @@ class chiamate{
 
 
 $chiamate=new chiamate();
+$chiamate->launcher();
 
 
 
 
 
 
- ?>
+?>
