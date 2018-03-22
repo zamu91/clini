55f87f86f655caa8fbfb5bb570a810a5120bc7e7
zamu <zamu@localhost.localdomain>
2018-03-22 18:33:22 +0100

    prep fix

--- portale/core/trait.login.php
+++ portale/core/trait.login.php
@@ -40,7 +40,7 @@ trait login {
   private function setIdArxivar(){
     echo "vai!";
     $username=$this->getUsername();
-    $this->queryPrepare(" SELECT UTENTE from dm_utente where description= ':user' ");
+    $this->queryPrepare(" SELECT UTENTE from dm_utente where description= :user ");
     $this->queryBind("user",$username);
     $this->executeQuery();
     $row=$this->fetch();
@@ -58,7 +58,7 @@ trait login {
     $que = "SELECT  ARXSESSION,
     TO_CHAR(SCADENZA, 'YYYY-MM-DD HH24:MI:SS') AS SCADENZA
     FROM XDM_WEBSERVICE_SESSION
-    WHERE USERNAME = ':userName' AND PASSWORD = ':password' ";
+    WHERE USERNAME = :userName AND PASSWORD = :password ";
     // $res = $this->query($que);
     $this->queryPrepare($que);
     $this->queryBind("userName", $userName);
