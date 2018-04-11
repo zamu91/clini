935f2cb15a7590a40a87f3de99d9c5219fecb85b
zamu <zamu@localhost.localdomain>
2018-04-11 17:49:16 +0200

    next manale

--- portale/core/trait.clinica.php
+++ portale/core/trait.clinica.php
@@ -5,13 +5,25 @@
 */
 trait clinica
 {
+
+  private function getNexIdAmbulatorio(){
+    $this->query("SELECT max(IDAMBULATORIO) as id from XDM_AMBULATORIO  ");
+    $row=$row=$this->fetch();
+    $id=$row['id'];
+    if(empty($id)){
+      $id=1;
+    }else{
+      $id++;
+    }
+    return $id;
+  }
+
   public function salvaClinica(){
     $data=$this->post('data');
-    $data['IDAMBULATORIO']='sequence.nexvalue';
+    $data['IDAMBULATORIO']=$this->getNexIdAmbulatorio();
     $this->insertPrepare('XDM_AMBULATORIO',$data);
     $this->commit();
     $this->setJsonMess("mess","Dati inseriti");
-
     $this->setJsonMess("ok",true);
   }
 
