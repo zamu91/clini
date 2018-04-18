6a95aa64334143c47e3499b4df1fbbfe8d92f17d
zamu <zamu@localhost.localdomain>
2018-04-18 15:40:56 +0200

    Init insContratto

--- portale/admin/js/admin.js
+++ portale/admin/js/admin.js
@@ -19,7 +19,81 @@ function getOptionClinica(idElement){
 
 function salvaContratto(){
   j={};
+  data={};function insClinica(){
+  window.location = 'insClinica.php';
+}
+
+
+function insContratto(){
+  window.location = 'insContratto.php';
+}
+
+
+
+function getOptionClinica(idElement){
   data={};
+  data.azione='getOptionClinica';
+  doLoad(idElement,data);
+}
+
+
+
+function salvaContratto(){
+  j={};
+  data={};
+  data.TEMPO=$('#durata').val();
+  data.IDAMBULATORIO=$('#idAmbulatorio').val();
+  data.DATAINIZIOCONTRATTO=$('#dataInizio').val();
+  data.DATAFINECONTRATTO=$('#dataFine').val();
+  data.ORAINIZIO=$('#oraInizio').val();
+  data.ORAFINE=$('#oraFine').val();
+  data.VERSO='1';
+  j.data=data;
+  j.giorni=getGiorniContratto();
+  j.azione='insContratto';
+  doAjax(j,function(mess){
+    alert('Contratto salvata con successo');
+  });
+}
+
+
+
+
+function getValGiorni(nome){
+  if ($('#'+nome).is(':checked')) {
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+function getGiorniContratto(){
+  giorni={};
+  giorni[1]=getValGiorni('lun');
+  giorni[2]=getValGiorni('mar');
+  giorni[3]=getValGiorni('mer');
+  giorni[4]=getValGiorni('gio');
+  giorni[5]=getValGiorni('ven');
+  giorni[6]=getValGiorni('sab');
+  return giorni;
+}
+
+
+function salvaAmbulatorio(){
+  data={};
+  add={};
+  add.NOME=$('#nomeAmbulatorio').val();
+  add.PROVINCIA=$('#provinciaAmbulatorio').val();
+  add.COMUNE=$('#comuneAmbulatorio').val();
+  add.INDIRIZZO=$('#indirizzoAmbulatorio').val();
+
+  data.data=add;
+  data.azione="salvaClinica";
+  doAjax(data,function(mess){
+    alert('Clinica salvata con successo');
+  });
+}
+
   data.TEMPO=$('#durata').val();
   data.IDAMBULATORIO=$('#idAmbulatorio').val();
   data.DATAINIZIOCONTRATTO=$('#dataInizio').val();
