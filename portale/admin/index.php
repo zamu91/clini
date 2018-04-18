5fa4aa0289c6d9b2e15bea843e9997c1e897af50
zamu <zamu@localhost.localdomain>
2018-04-18 16:14:55 +0200

    html

--- portale/admin/index.php
+++ portale/admin/index.php
@@ -5,21 +5,31 @@ include_once 'template/head.php';
 <body>
   <?php
   getHeader("Amministrazione clinica","Seleziona l'azione da eseguire");
+  getSubHeader();
   ?>
 
   <section class="container">
     <div class="columns features">
-      <div class="column is-4">
+
+      <div class="column is-6">
         <div class="card is-shady">
-          <h2>Procedura di inserimento di una nuova clinica</h2>
-          <button class="button" onclick="insClinica();">INSERISCI CLINICA</button>
+          <div class="card-image has-text-centered">
+            <h2>Procedura di inserimento di una nuova clinica</h2>
+          </div>
+          <div class="card-content">
+            <button class="button" onclick="insClinica();">INSERISCI CLINICA</button>
+          </div>
         </div>
       </div>
 
-      <div class="column is-4">
+      <div class="column is-6">
         <div class="card is-shady">
-          <h2>Procedura di inserimento di un nuovo contratto</h2>
-          <button class="button" onclick="insContratto();">INSERISCI CONTRATTO</button>
+          <div class="card-image has-text-centered">
+            <h2>Procedura di inserimento di un nuovo contratto</h2>
+          </div>
+          <div class="card-content">
+            <button class="button" onclick="insContratto();">INSERISCI CONTRATTO</button>
+          </div>
         </div>
       </div>
     </div> <!-- end columns feauteres -->
