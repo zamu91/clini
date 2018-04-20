7fb9fd5f4b3e84084bb5dd118c90335c6306b905
Vito Pastrello <vito@localhost.localdomain>
2018-04-20 18:49:42 +0200

    completamento caricamento file in maschera iniziale

--- portale/js/azioni.js
+++ portale/js/azioni.js
@@ -179,9 +179,21 @@ function navigaDashboard(){
 
 
 function scriviDatiProfilo(){
-  var jd = $("#formMaschera").serialize();
-  jd += "&azione=scriviDatiProfilo";
-  jd += "&maskIx="+$("#maskIx").val();
+  var jd = {};
+  jd.azione = "scriviDatiProfilo";
+  jd.maskIx = $("#maskIx").val();
+  jd.COMBO15_297 = $("#COMBO15_297").val();
+  jd.COMBO19_1 = $("#COMBO19_1").val();
+  jd.TESTO10_297 = $("#TESTO10_297").val();
+  jd.TESTO13_297 = $("#TESTO13_297").val();
+  jd.CHECK17_1 = $("#CHECK17_1").val();
+  jd.TESTO14_297 = $("#TESTO14_297").val();
+  jd.TESTO12_297 = $("#TESTO12_297").val();
+  var file = [];
+  $("#files").children(".uploadedFile").each(function(){
+    file.push($(this).data("info"));
+  });
+  jd.files = file;
   console.log(jd);
   doAjax(jd, function(mess){
     $("#requestResult").html(mess);
