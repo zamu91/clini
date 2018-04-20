f734a62fc307b7e0694eb33f90d8f75a97b03097
zamu <zamu@localhost.localdomain>
2018-04-20 18:15:17 +0200

    dtamaskix

--- portale/utente/menuUtente.php
+++ portale/utente/menuUtente.php
@@ -10,12 +10,12 @@ include_once '../admin/template/html.php';
 
  <div class="container">
    <div class="columns">
-     <?php divElement('<button class="button" onclick="apriProfilo("0");">Responsabilità civile auto</button>',"Inserisci Codice patrocinatore","6"); ?>
-     <?php divElement('<button class="button" onclick="apriProfilo("1");">Responsabilità civile terzi</button>',"Inserisci Codice patrocinatore","6"); ?>
+     <?php divElement('<button class="button" data-maskix="0" onclick="apriProfilo(sender);">Responsabilità civile auto</button>',"Inserisci Codice patrocinatore","6"); ?>
+     <?php divElement('<button class="button" data-maskix="0" onclick="apriProfilo(sender);">Responsabilità civile terzi</button>',"Inserisci Codice patrocinatore","6"); ?>
    </div>
    <div class="columns">
-     <?php divElement('<button class="button" onclick="apriProfilo("2");">Polizza privata infortuni</button>',"Inserisci Codice patrocinatore","6"); ?>
-     <?php divElement('<button class="button" onclick="apriProfilo("3");">Consulenza tecnica di parte</button>',"Inserisci Codice patrocinatore","6"); ?>    
+     <?php divElement('<button class="button" data-maskix="0" onclick="apriProfilo(sender);">Polizza privata infortuni</button>',"Inserisci Codice patrocinatore","6"); ?>
+     <?php divElement('<button class="button" data-maskix="0" onclick="apriProfilo(sender);">Consulenza tecnica di parte</button>',"Inserisci Codice patrocinatore","6"); ?>
    </div>
 
  </div>
