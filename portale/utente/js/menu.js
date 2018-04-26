a79cc5ffede7accc5a9205df5f6ed4659b29afb2
zamu <zamu@localhost.localdomain>
2018-04-26 11:37:16 +0200

    Set up cerca clinica

--- portale/utente/js/menu.js
+++ portale/utente/js/menu.js
@@ -3,8 +3,47 @@ $( document ).ready(function() {
   jd.azione='controlloTokenARXLogin';
   doAjax(jd,function(data){
     if(data.validToken){
+      loadProvClinica();
     }else{
       goIndexUtente();
     }
   });
 });
+
+
+function loadProvClinica(){
+    data={};
+    data.azione='optClinicaProvincia';
+    doLoad('#clinicaCerca',data);  
+}
+
+
+function cercaClinica(tipo){
+
+  if(tipo=='data'){
+    $('.cercaPerData').show('slow');
+    $('.cercaPerProv').hide('slow');
+    return 0;
+  }
+  if(tipo=='provincia'){
+    $('.cercaPerData').hide('slow');
+    $('.cercaPerProv').show('slow');
+    return 0;
+  }
+}
+
+function cercaPerClinica(){
+  j={};
+  j.clinica=$('#clinicaCerca').val();
+  j.azione='getDataPerClinica';
+  doLoad('.resultClinica',j);
+}
+
+
+
+function cercaPerData(){
+  j={};
+  j.data=$('#clinicaCerca').val();
+  j.azione='getClinicaPerData';
+  doLoad('#resultClinica',data);
+}
--- portale/utente/menuUtente.php
+++ portale/utente/menuUtente.php
@@ -3,20 +3,56 @@ include_once '../template/head.php';
 include_once '../admin/template/html.php';
 //include_once '../core/class.chiamate.php';
 //$chiamate->controlloArxLogin();
- ?>
- <body>
-   <script src="js/navigazione.js"></script>
-   <script src="js/menu.js?v=1"></script>
+?>
+<body>
+  <script src="js/navigazione.js"></script>
+  <script src="js/menu.js?v=1"></script>
 
- <div class="container">
-   <div class="columns">
-     <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Responsabilità civile auto</button>',"Inserisci Codice patrocinatore","6"); ?>
-     <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Responsabilità civile terzi</button>',"Inserisci Codice patrocinatore","6"); ?>
-   </div>
-   <div class="columns">
-     <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Polizza privata infortuni</button>',"Inserisci Codice patrocinatore","6"); ?>
-     <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Consulenza tecnica di parte</button>',"Inserisci Codice patrocinatore","6"); ?>
-   </div>
+  <div class="container">
 
- </div>
+    <div class="cercaClinica">
+      <div class="radioOpzioni">
+        <label><input type="radio" name="tipo" value="data" onclick="cercaClinica('data');" > Cerca Clinica per data</label>
+        <label><input type="radio" name="tipo" value="posizione" onclick="cercaClinica('provincia');">Cerca Clinica per posizione</label>
+      </div> <!-- radioOpzioni -->
+
+
+      <div class="cercaPerData">
+        <div class="inputDiv">
+          <label for="dataCercaClinica">Data</label>
+          <input type="edit" class="data input" id="dataCercaClinica">
+        </div>
+        <button class="button is_primary" onclick="cercaPerProvincia();">Cerca</button>
+      </div> <!-- cerca per data -->
+
+      <div class="cercaPerProv">
+        <div class="inputDiv">
+          <label for="clinica">Clinica</label>
+          <select class="input" class="select" id="clinicaCerca">
+          </select>
+        </div>
+        <button class="button is_primary" onclick="cercaPerClinica();">Cerca</button>
+      </div>
+
+      <div class="resultClinica">
+
+      </div> <!-- end res Clinica-->
+
+
+    </div><!-- fine cerca X clinica -->
+
+
+
+    <div class="tipoPrenotazione">
+
+      <div class="columns">
+        <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Responsabilità civile auto</button>',"Inserisci Codice patrocinatore","6"); ?>
+        <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Responsabilità civile terzi</button>',"Inserisci Codice patrocinatore","6"); ?>
+      </div>
+      <div class="columns">
+        <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Polizza privata infortuni</button>',"Inserisci Codice patrocinatore","6"); ?>
+        <?php divElement('<button class="button is-primary" data-maskix="0" onclick="apriProfilo(this);">Consulenza tecnica di parte</button>',"Inserisci Codice patrocinatore","6"); ?>
+      </div>
+    </div> <!-- end tipo prenotazione -->
+  </div><!-- end container -->
 </body>
