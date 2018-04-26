589e5936ccf5c726b425768be7518ae8f0feef53
Vito Pastrello <vito@localhost.localdomain>
2018-04-26 17:41:12 +0200

    fsb

--- portale/core/trait.arxivar.php
+++ portale/core/trait.arxivar.php
@@ -90,7 +90,7 @@ trait arxivar{
       ?>
       <form id="formMaschera" action="#" onsubmit="function(){ return false; }" method="post">
         <fieldset>
-          <legend>Maschera</legend>
+          <legend></legend>
           <?php
           foreach ($details as $field) {
 
@@ -217,6 +217,7 @@ trait arxivar{
 
     $this->logoutArxivar();
     return ob_get_clean();
+    die();
   }
 
   public function dettaglioProfilo(){
@@ -322,6 +323,7 @@ trait arxivar{
     <?php
 
     $this->logoutArxivar();
+    die();
   }
 
   public function getTaskworkFromDocnumber(){
