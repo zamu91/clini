89c13064ee67e5c089ff622e0a14eb17d628c73a
zamu <zamu@localhost.localdomain>
2018-05-04 17:07:33 +0200

    init login

---
+++ portale/patrocinatore/index.php
@@ -0,0 +1,29 @@
+<?php
+include_once '../template/head.php';
+include_once '../admin/template/html.php';
+?>
+<body>
+  <script src="js/navigazione.js"></script>
+  <div id="container">
+    <div class="columns">
+      <div class="column is-one-quarter"></div>
+      <div class="column is-half">
+        <br>
+        <h1 class="title">Login</h1>
+        <div class="fieldBox">
+          <label>Username</label><br>
+          <input type="text" id="username" name="an_username" placeholder="Username"/>
+        </div>
+        <div class="fieldBox">
+          <label>Password</label><br>
+          <input type="text" id="password" name="an_password" placeholder="Password" />
+        </div>
+        <div style="clear:both;"></div>
+        <br>
+        <button class="button is-primary" onclick="login();">Login</button>
+      </div>
+      <div class="column is-one-quarter"></div>
+    </div>
+  </div>
+</body>
+</html>
