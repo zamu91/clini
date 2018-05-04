b4f08fadffe166fe4652eb4c04fac8fa5f553cf0
zamu <zamu@localhost.localdomain>
2018-05-04 17:34:11 +0200

    Nuovi elementi html

--- portale/admin/template/html.php
+++ portale/admin/template/html.php
@@ -1,32 +1,52 @@
 <?php
 
-function divElement($element,$label,$col){
+
+
+function divElementOld($element,$label,$col){
   ?>
   <div class="column is-<?php echo $col; ?>">
 
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">
-          <?php echo $label; ?>
-        </p>
-
-      </header>
-      <div class="card-content">
-        <div class="content">
-          <div class="control has-icons-left has-icons-right">
-            <?php echo $element; ?>
-          </div>
-        </div>
-      </div>
+    <div class="element">
+      <label class="label">
+        <?php echo $label; ?>
+      </label><br>    
+      <?php echo $element; ?>
     </div>
-
   </div>
 
   <?php
 
 
-}
+
+  function divElementOld($element,$label,$col){
+    ?>
+    <div class="column is-<?php echo $col; ?>">
+
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            <?php echo $label; ?>
+          </p>
+
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div class="control has-icons-left has-icons-right">
+              <?php echo $element; ?>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <?php
 
 
 
-?>
+
+  }
+
+
+
+  ?>
