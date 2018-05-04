dd698b726fda488fda06e1615ac271738d3fb586
zamu <zamu@localhost.localdomain>
2018-05-04 11:46:26 +0200

    css e js

---
+++ portale/patrocinatore/js/menu.js
@@ -0,0 +1,112 @@
+var idPrenotazioneSelect=null;
+var dataSelect=null;
+
+
+$( document ).ready(function() {
+  jd={};
+  jd.azione='controlloTokenARXLogin';
+  doAjax(jd,function(data){
+    if(data.validToken){
+      loadProvClinica();
+      datPick('#dataCerca');
+    }else{
+      goIndexUtente();
+    }
+  });
+});
+
+
+
+function cercaPerClinica(){
+  j={};
+  j.clinica=$('#clinicaCerca').val();
+  j.azione='getDataPerClinica';
+  doLoad('.resultClinica',j);
+}
+
+function cercaPerData(){
+  j={};
+  j.data=$('#dataCerca').val();
+  j.azione='getClinicaPerData';
+  doLoad('.resultClinica',j);
+}
+
+
+function loadProvClinica(){
+  data={};
+  data.azione='optClinicaProvincia';
+  doLoad('#clinicaCerca',data);
+}
+
+
+function cercaClinica(tipo){
+
+  if(tipo=='data'){
+    $('.cercaPerData').show('slow');
+    $('.cercaPerProv').hide('slow');
+    return 0;
+  }
+  if(tipo=='provincia'){
+    $('.cercaPerData').hide('slow');
+    $('.cercaPerProv').show('slow');
+    return 0;
+  }
+}
+
+
+
+
+function scegliPrenotazione(id,data){
+  idPrenotazioneSelect=id;
+  dataSelect=data;
+  $('.cercaClinica').hide('slow');
+  $('.tipoPrenotazione').show('slow');
+}
+
+
+
+function apriProfiloImpersonate(sender, newdoc){
+
+  var docnumber = ( typeof newdoc != 'undefined' && !newdoc) ? getDocunumberDashboard() : "";
+  var jd = { azione: "dettaglioProfilo", docnumber: docnumber, maskix: $(sender).data("maskix") };
+  doLoad(".maschera", jd, function(){
+    $('.tipoPrenotazione').hide('slow');
+    $('.maschera').show('slow');
+
+    $("#modal-title").html( $(sender).html() );
+    $("#modal-action").modal("toggle");
+    $('.maschera').append('<button class="is-primary button" '+
+    ' onclick="scriviDatiProfiloImpersonate();">SALVA PROFILO</button>');
+  });
+}
+
+
+
+
+function scriviDatiProfiloImpersonate(){
+  var jd = {};
+  alert('innizio ');
+  jd.azione = "scriviDatiProfilo";
+  jd.maskIx = $("#maskIx").val();
+  jd.COMBO15_297 = $("#COMBO15_297").val();
+  jd.COMBO19_1 = $("#COMBO19_1").val();
+  jd.TESTO10_297 = $("#TESTO10_297").val();
+  jd.TESTO13_297 = $("#TESTO13_297").val();
+  jd.CHECK17_1 = $("#CHECK17_1").val();
+  jd.TESTO14_297 = $("#TESTO14_297").val();
+  jd.TESTO12_297 = $("#TESTO12_297").val();
+  jd.idContratto=idPrenotazioneSelect;
+  jd.data=dataSelect;
+  var file = [];
+  jd.files = file;
+  alert('fine data');
+  doAjax(jd, function(data){
+    if(data.res){
+      swal('Prenotaizone','Prenotazione avvenuta con successo','success');
+    } else {
+      swal("Errore","Salvataggio profilazione fallito.","error");
+    }
+  }, function(jqXHR, textStatus, errorThrown){
+    swal("errore","Errore salvataggio profilazione.","error");
+  });
+}
---
+++ portale/patrocinatore/js/navigazione.js
@@ -0,0 +1,33 @@
+urlAjax="../core/class.chiamate.php";
+
+
+
+function accediCercaLibero(){
+  var code = $('#codPatro').val();
+  data={ azione: "loginImpersonatePatrocinatore", code: code};
+  doAjax(data,function(data, textStatus){
+    if(data.login){
+      setToken(data.token);
+      accediMenu();
+
+    } else {
+      swal("Errore","Codice patrocinatore non trovato","error");
+    }
+  });
+
+}
+
+function accediMenu(){
+  window.location.href="menuUtente.php";
+}
+
+function goIndexUtente(){
+  window.location.href="utente.php";
+}
+
+
+
+
+function accediRicerca(){
+
+}
