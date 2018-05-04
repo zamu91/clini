8e27b65177b835e8f99bfa761f002a89ea5c228e
zamu <zamu@localhost.localdomain>
2018-05-04 18:22:22 +0200

    rimesso il parametro

--- portale/patrocinatore/js/menu.js
+++ portale/patrocinatore/js/menu.js
@@ -108,6 +108,7 @@ function scriviDatiProfilo(){
   jd.azione = "scriviDatiProfilo";
   jd.maskIx = $("#maskIx").val();
   jd.data=dataSelect;
+  jd.idContratto=idPrenotazioneSelect;
   jd.COMBO15_297 = $("#COMBO15_297").val();
   jd.COMBO19_1 = $("#COMBO19_1").val();
   jd.TESTO10_297 = $("#TESTO10_297").val();
