2ae9bf82815d74b1bf6fb934bc0b12807a941d09
zamu <zamu@localhost.localdomain>
2018-05-07 22:28:42 +0200

    indietro da pulsanti

--- portale/patrocinatore/inserisciPrenotazione.php
+++ portale/patrocinatore/inserisciPrenotazione.php
@@ -73,6 +73,8 @@ include_once '../admin/template/html.php';
         <?php divElement('<button class="button is-primary" data-maskix="2" onclick="apriProfilo(this);">Polizza privata infortuni</button>',"","6"); ?>
         <?php divElement('<button class="button is-primary" data-maskix="3" onclick="apriProfilo(this);">Consulenza tecnica di parte</button>',"","6"); ?>
       </div>
+      <button class="button is-primary" onclick="indietroBottoni();">Indietro</button>
+
     </div> <!-- end tipo prenotazione -->
 
 
