88c5782bf118396888fc213e30cdd998b9adafab
zamu <zamu@localhost.localdomain>
2018-05-09 18:31:57 +0200

    chekc mail utente

--- portale/utente/js/menu.js
+++ portale/utente/js/menu.js
@@ -81,9 +81,20 @@ function apriProfiloImpersonate(sender, newdoc){
 
 
 
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
 
 function scriviDatiProfiloImpersonate(){
   var jd = {};
+  if(!validateEmail($('#TESTO12_297').val())){
+    swal("warning","Mail non valida, inserirne una correttamente","warning");
+    return 0;
+  }
+
+
   jd.azione = "scriviDatiProfilo";
   jd.maskIx = $("#maskIx").val();
   jd.COMBO15_297 = $("#COMBO15_297").val();
