b5142f01acee1f4c674b5e3e85d93626fba81ec7
zamu <zamu@localhost.localdomain>
2018-05-11 17:57:55 +0200

    .

--- portale/core/trait.navigazione.php
+++ portale/core/trait.navigazione.php
@@ -55,14 +55,14 @@ trait navigazione {
 
       <div id="modal-action" class="modal" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
-          <div class="modal-content">
+          <div class="modal-content ">
             <div class="modal-header">
-              <h5 id="modal-title" class="modal-title">Modal title</h5>
+              <h5 id="mask-title" class="modal-title ">Modal title</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div id="modal-body" class="modal-body">
+            <div id="modal-body" class="modal-body maschera">
               <p>Modal body text goes here.</p>
             </div>
             <div class="modal-footer">
