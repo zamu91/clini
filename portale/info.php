dd8fc08d6ea756403be1b8bd08394500721df14a
zamu <zamu@localhost.localdomain>
2018-05-11 16:00:47 +0200

    .

--- portale/info.php
+++ portale/info.php
@@ -5,6 +5,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once 'config/conOCI.php';
 $ora=new conOCI();
+if(!$ora){
+  echo "NON VA NIENTE!!!<br>";
+}
 echo "oggetto";
 $ora->connettiOracle();
 echo "connesso";
