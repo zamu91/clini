85935dd0eeb89c9ea9712de7161c7e2493e269bc
zamu <zamu@localhost.localdomain>
2018-05-11 16:01:53 +0200

    .

--- portale/info.php
+++ portale/info.php
@@ -5,11 +5,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once 'config/conOCI.php';
 $ora=new conOCI();
-if(!$ora){
-  echo "NON VA NIENTE!!!<br>";
-}
 echo "oggetto";
-$ora->connettiOracle();
+$res=$ora->connettiOracle();
+if(!$res){
+  echo "NON VA NIENTE!!!<br>";
+}else{
+  echo "ok sembra connesso";
+}
+
 echo "connesso";
 $res=$ora->query("select * from XDM_WEBSERVICE_SESSION");
 echo "queyr";
