7e69cbdd66f1f69434e535e417a8cdcedcf943a3
Vito Pastrello <vito@localhost.localdomain>
2018-05-11 17:43:35 +0200

    s

--- portale/core/jquery-file-upload-9.21.0/UploadHandler.php
+++ portale/core/jquery-file-upload-9.21.0/UploadHandler.php
@@ -45,9 +45,9 @@ class UploadHandler
       // echo $this->get_full_url().'/uploads/';
       // die;
 
-      echo dirname($this->get_server_var('DOCUMENT_ROOT')).'/html/arx_portale/clini/portale/uploads/';
-      echo dirname($this->get_server_var('SERVER_NAME')).'/html/arx_portale/clini/portale/uploads/';
-      die;
+      // echo dirname($this->get_server_var('DOCUMENT_ROOT')).'/html/arx_portale/clini/portale/uploads/';
+      // echo dirname($this->get_server_var('SERVER_NAME')).'/html/arx_portale/clini/portale/uploads/';
+      // die;
 
         $this->response = array();
         $this->options = array(
