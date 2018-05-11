35827709270bd3d1b152bb77d833a646fc14cce7
zamu <zamu@localhost.localdomain>
2018-05-11 15:57:45 +0200

    .

--- portale/info.php
+++ portale/info.php
@@ -1,4 +1,10 @@
 <?php
-phpinfo();
-
+include_once 'config/conOCI.php';
+$ora=new conOCI();
+echo "oggetto";
+$ora->connettiOracle();
+echo "connesso";
+$res=$ora->query("select * from XDM_WEBSERVICE_SESSION");
+echo "queyr";
+print_r($res);
  ?>
