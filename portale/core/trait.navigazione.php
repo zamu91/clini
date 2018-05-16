46eaf95dcfe17e15b6dcbc17dbe9e63c8282ca4b
Vito Pastrello <vito@localhost.localdomain>
2018-05-16 12:02:07 +0200

    margin, estetica

--- portale/core/trait.navigazione.php
+++ portale/core/trait.navigazione.php
@@ -50,8 +50,8 @@ trait navigazione {
       <div class="clearSpace"></div>
 
       <div id="containerComandi" class="hidden dashSection" data-task="">
-        <button class="button is-primary" onclick="apriProfilo(this, false);">Carica doc.</button>
-        <button class="button is-primary" onclick="open_preview()" title="preview">Anteprima</button>
+        <button class="button is-primary" onclick="apriProfilo(this, false);" style="margin-bottom: 5px;" title="Carica documento">Carica doc.</button>
+        <button class="button is-primary" onclick="open_preview()" style="margin-bottom: 5px;" title="preview">Anteprima</button>
         <button class="button is-primary" onclick="open_download()" title="download">Download</button>
       </div>
       <div id="containerDocumenti" class="hidden dashSection" data-task=""></div>
