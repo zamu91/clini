3cd48f37b442a0ba3bff2925672dd8cbeeb97caf
zamu <zamu@localhost.localdomain>
2018-05-16 19:12:16 +0200

    add beetween

--- portale/core/trait.contratto.php
+++ portale/core/trait.contratto.php
@@ -107,8 +107,9 @@ trait contratto{
     $adCol=", ( $adCol ) as DAYCONF";
 
     $str="SELECT XDM_AMBULATORIO_CONTRATTO.IDCONTRATTO $adCol  FROM XDM_AMBULATORIO_CONTRATTO $adJoin
-    where IDAMBULATORIO=:idAmb and (DATAINIZIOCONTRATTO>=$iniz or DATAFINECONTRATTO<=$fine)
-    and (ORAINIZIO>=:oraIniz or ORAFINE<:oraFine)    ";
+    where IDAMBULATORIO=:idAmb and ($iniz BETWEEN DATAINIZIOCONTRATTO AND DATAFINECONTRATTO
+      or $fine BETWEEN DATAFINECONTRATTO AND DATAFINECONTRATTO   )
+    and (:oraIniz BETWEEN ORAINIZIO and ORAFINE or :oraFine BETWEEN ORAINIZIO and ORAFINE)    ";
 
     $this->queryPrepare($str);
     $this->queryBind('dataIniz',$this->dataInizioSTR);
