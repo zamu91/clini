e3779f7214ed2ccf257c255f72249b54ac4e1ff1
zamu <zamu@localhost.localdomain>
2018-05-23 19:24:50 +0200

    .

--- portale/js/azioni.js
+++ portale/js/azioni.js
@@ -135,7 +135,6 @@ function dettagliTaskProfilo(target){
 
 function selezionaDocumento(target){
   $(target).parent("tbody").children("tr.is-selected").removeClass("documentSelectTask");
-
   $(target).parent("tbody").children("tr.is-selected").removeClass("is-selected");
 
   $(target).addClass("is-selected documentSelectTask");
